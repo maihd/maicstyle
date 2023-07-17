09f1cb97d1532daf047adcf28b822210c8a7d460
maihd <maihd.dev@gmail.com>
2023-07-17 09:07:28 +0700

    ex1_5.c

---
+++ exercises/kr_c_book/chap1/ex1_5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/// Print Fahrenheit-Celsius table
+/// for fahr = 0, 20, ..., 300;
+/// floating-point version
+int main()
+{
+    const int lower = 0;
+    const int upper = 300;
+    const int step  = 20;
+
+    printf("| Fahr | Celsius |\n");
+    printf("|------|---------|\n");
+    for (float fahr = (float)upper; fahr >= (float)lower; fahr -= step)
+    {
+        const float celsius = (5.0f / 9.0f) * (fahr - 32.0f);
+        printf("| %4.0f | %7.1f |\n", fahr, celsius);
+    }
+
+    return 0;
+}
+
+//! EOF
