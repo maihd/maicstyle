bee89daabd879b84f33079642ad34b0a2a7d9c1b
maihd <maihd.dev@gmail.com>
2024-01-23 10:09:55 +0700

    update defer_and_scope.c

--- examples/defer_and_scope/defer_and_scope.c
+++ examples/defer_and_scope/defer_and_scope.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
-#define macro_var_2(i, l)   i ## l
-#define macro_var_1(i, l)   macro_var_2(i, l)
-#define macro_var(i)        macro_var_1(i, __LINE__)
-#define defer(expr)         for (int macro_var(__i_) = 0; macro_var(__i_) == 0; macro_var(__i_)--, expr)
-#define scope(init, deinit) for (int macro_var(__i_) = (init, 0); macro_var(__i_) == 0; macro_var(__i_)--, deinit)
+#define __macro_var_2(i, l)   i ## l
+#define __macro_var_1(i, l)   __macro_var_2(i, l)
+#define __macro_var(i)        __macro_var_1(i, __LINE__)
+#define __defer(expr)         for (int __macro_var(__i_) = 0; __macro_var(__i_) == 0; __macro_var(__i_)--, expr)
+#define __scope(init, deinit) for (int __macro_var(__i_) = (init, 0); __macro_var(__i_) == 0; __macro_var(__i_)--, deinit)
 
 
 int main()
 {
-    defer(printf("defer showcase: End of block!\n"))
+    __defer(printf("__defer showcase: End of block!\n"))
     {
-        printf("defer showcase: Start of block!\n");
+        printf("__defer showcase: Start of block!\n");
     }
 
     printf("-------------------------------\n");
 
-    scope(
-        printf("scope showcase: Init phrase!\n"),
-        printf("scope showcase: Deinit phrase!\n")
+    __scope(
+        printf("__scope showcase: Init phrase!\n"),
+        printf("__scope showcase: Deinit phrase!\n")
     )
     {
-        printf("scope showcase: Do work...\n");
+        printf("__scope showcase: Do work...\n");
     }
 
     return 0;
