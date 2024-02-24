393db7771be0aaaf1785f822fb1931ce915b300a
maihd.dev <maihd.dev@gmail.com>
2024-02-25 00:28:02 +0700

    fix examples/math2d_101/types.h

--- exercises/math2d_101/types.h
+++ exercises/math2d_101/types.h
@@ -5,9 +5,9 @@ typedef int             i32;
 typedef unsigned int    u32;
 typedef float           f32;
 
-typedef i32             i32x2 __attribute__((vector_type(2)); // Vector2 of i32
-typedef u32             u32x2 __attribute__((vector_type(2)); // Vector2 of u32
-typedef f32             f32x2 __attribute__((vector_type(2)); // Vector2 of f32
+typedef i32             i32x2 __attribute__((ext_vector_type(2))); // Vector2 of i32
+typedef u32             u32x2 __attribute__((ext_vector_type(2))); // Vector2 of u32
+typedef f32             f32x2 __attribute__((ext_vector_type(2))); // Vector2 of f32
 
 //! EOF
 
