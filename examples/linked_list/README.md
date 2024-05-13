7a5bd4e2e05e09267b72278817206af6c2e57f4e
maihd <maihd.dev@gmail.com>
2024-05-13 16:13:01 +0700

    linked_list example

---
+++ examples/linked_list/README.md
@@ -0,0 +1,6 @@
+# Linked list example
+With good taste and cache-friendly processing pattern.
+
+## Additional reading
+- https://felipec.github.io/good-taste/parts/1.html
+- https://www.rfleury.com/p/in-defense-of-linked-lists
---
+++ examples/linked_list/main.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/// Arena
+/// Simple implementation which is the best for this example (not in practice/production)
+typedef struct Arena
+{
+    size_t  head;
+    size_t  tail;
+
+    void*   data;
+    size_t  size;
+} Arena;
+
+
+/// ListNode
+/// Generic node data structure implementation for using Linked List
+typedef struct ListNode
+{
+    struct ListNode* next;
+} ListNode;
+
+
+/// LIST_NULL_NODE 
+/// NULL in C is void*, which is untyped, may caused compile-error when using macro
+/// For more robust code, we can use the address of a predefined static variables node
+/// which will prevent null-pointer access, reduce null-pointer check before access node
+#define LIST_NULL_NODE ((ListNode*)0)
+
+
+/// container_of
+/// The famous Linux kernel's macro
+/// This macro is helpful when cast Member to Super data structure
+#define container_of(ptr, TSuper, member) ((TSuper*)((uint8_t*)(ptr) - offsetof(TSuper, member)))
+
+
+/// node_entry
+/// Another famous Linux kernel's macro
+/// This macro is helpful when cast ListNode to Entry data structure
+#define node_entry(ptr, Entry) container_of(ptr, Entry, node)
+
+
+/// new_node
+/// Create new node, memory get from Arena for cache-friendly accessing
+#define new_node(Type, arena, prev_node) (prev_node != LIST_NULL_NODE \
+    ? ((prev_node)->next = &((Type*)memset(acquire_memory(arena, sizeof(Type)), 0, sizeof(Type)))->node \
+    , node_entry((prev_node)->next, Type)) \
+    : (Type*)memset(acquire_memory(arena, sizeof(Type)), 0, sizeof(Type))\
+    )
+
+
+/// acquire_memory
+/// Get memory from Arena (no system memory allocation happen)
+void* acquire_memory(Arena* arena, size_t size)
+{
+    if (arena == NULL || arena->tail - arena->head < size)
+    {
+        return NULL;
+    }
+
+    void* result = (uint8_t*)arena->data + arena->head;
+    arena->head += size;
+    return result;
+}
+
+
+/// Test
+/// Data structure for testing
+typedef struct Test
+{
+    ListNode    node;   // Must be value (not pointer or crash)
+    int32_t     value;  // Simple value
+} Test;
+
+
+int main()
+{
+    printf("Linked list example!\n");
+
+    size_t buffer_size = 1024;
+    void* buffer_data = malloc(buffer_size);
+
+    Arena arena = {
+        .head = 0,
+        .tail = buffer_size,
+        .data = buffer_data,
+        .size = buffer_size,
+    };
+
+    Test* a = new_node(Test, &arena, LIST_NULL_NODE);
+    Test* b = new_node(Test, &arena, &a->node);
+    
+    a->value = 1;
+    b->value = 2;
+
+    for (ListNode* p = &a->node; p != NULL; p = p->next)
+    {
+        Test* v = node_entry(p, Test);
+        printf("Value of %p = %d\n", p, v->value); 
+        // You will see the address of node is nearly sitting next previous node
+        // That why this Linked List is cache friendly
+    }
+
+    // Cleanup
+    free(buffer_data); // The only needing to cleanup is to free buffer_data
+
+    return 0;
+}
+
+//! EOF
