20e0dc8316f53734f7ffff3f283545d7ff42f0a2
ZenFox <maihd.dev@gmail.com>
2025-08-30 15:15:09 +0400

    exploring more use cases of strings

--- tools/slibs/include/slibs/typedefs.h
+++ tools/slibs/include/slibs/typedefs.h
@@ -5,12 +5,43 @@
 #include <stdarg.h>
 #include <stdbool.h>
 
+
+typedef enum StrBufFlags
+{
+    StrBuf_None     = 0,
+    StrBuf_Fixed    = 1 << 0,
+    StrBuf_Owned    = 1 << 1,
+    StrBuf_Alloced  = 1 << 2,
+    StrBuf_ViewOnly = 1 << 3,
+} StrBufFlags;
+
+
 typedef struct StrBuf
 {
     char*       data;
     uint32_t    size;
     uint32_t    flags;
 } StrBuf;
+
+
+typedef struct StrBuf128
+{
+    union
+    {
+        StrBuf  strbuf;
+        char*   data;  
+    };
+
+    char fixed_buffer[128];
+} StrBuf128;
+
+
+typedef struct StrView
+{
+    char*    data;
+    uint32_t size;
+} StrView;
+
 
 //! EOF
 
